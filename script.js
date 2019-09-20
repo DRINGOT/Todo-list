82623babf6e9c0182982caf38cbc07dc6b686167
DRINGOT <donovan.ringot@hotmail.fr>
2019-09-20 16:24:43 +0200

    create symbol delete in script.js

--- script.js
+++ script.js
@@ -1,52 +1,67 @@
-'use strict'
+'use strict';
 // variables
 let button = document.getElementById("enter");
 let input = document.getElementById("userinput");
 let ul = document.querySelector("ul");
 let li;
 
-//Functions
+//Functions 
 function inputLength() {
     return input.value.length;
 }
 
-function resetInput() {
-    input.value = "";
-}
-
-function addTextNode() {
-    li.append(document.createTextNode(CapitalizeFirstLetterInput()));
-}
 function createNewLi() {
     li = document.createElement("li");
+}
+
+function addTextNodeInLi() {
+    li.append(document.createTextNode(Capitalize()));
 }
 
 function insertLi() {
     ul.insertAdjacentElement("beforeend", li);
 }
 
+function resetInput() {
+    input.value = "";
+}
+//Function Global
 function createListElement() {
     createNewLi();
-    addTextNode();
+    addTextNodeInLi();
     insertLi();
     resetInput();
+    symbolDelete();
+
+}
+//Function delete
+function symbolDelete() {
+    let deleteButton = document.createElement("button");
+    let cross = document.createTextNode("\u00D7");
+    span.className = "delete";
+    span.append(cross);
+    li.append(deleteButton);
 }
 
+
+//Functions validation add
 function addListAfterClick() {
     if (inputLength() > 0) {
         createListElement();
     }
 }
 
-function addListAfterKeyPress(event) {
-    if (inputLength() > 0 && event.keyCode === 13) {
+function addListAfterKeyPress(key) {
+    if (inputLength() > 0 && key.keyCode === 13) {
         createListElement();
     }
 }
 
-function CapitalizeFirstLetterInput() {
+//function Text Style
+function Capitalize() {
     return input.value[0].toUpperCase() + input.value.slice(1, inputLength()).toLowerCase();
 }
+
 // Processus
 button.addEventListener("click", addListAfterClick);
 
